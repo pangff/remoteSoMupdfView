88b42d5b5a5af39fb58353182fb3bc070890fe79
“pangff” <admin@example.com>
2016-08-29 10:45:02 +0800

    add cpu abi check

--- app/src/main/java/com/jhss/romtesomupdf/MainActivity.java
+++ app/src/main/java/com/jhss/romtesomupdf/MainActivity.java
@@ -85,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
     public void downPdfPlugin(final View view) {
 
         PdfManager.getInstance()
-                .download("http://download.youguu.com/download/plugs/libmupdf_java.so.zip",
+                .download("http://download.youguu.com/download/",
                         new FileCallback(Environment.getExternalStorageDirectory() +
                                 "/jhss/pdf", "libmupdf_java.so.zip") {
                             @Override
